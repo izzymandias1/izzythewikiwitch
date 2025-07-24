aa67ed3b1d85d7c23cebc37043627b91c25f4fb7
izzymandias1 <hazardfirefly@gmail.com>
2025-07-24 19:56:50 +0100

    added js files, footer and header files, good lord. lots of stuff. finished portfolio

--- header.js
+++ header.js
@@ -1,13 +1,13 @@
     let appHeader = `
-    <header>
+    <header class="headertext">
         <img class="logo" src="images/2024_PFP.png"
             alt="izzy's logo for 2024 to 2025. a picture of a woman with deer antlers in front of the lesbian flag."
             width="200" />
         <h1 class="title">izzythewikiwitch</h1>
         <sub>"what if i made my own wiki. with yuri. and love."</sub>
         <div class="topnav">
-            <a href="index.html">home</a>
-            <a href="portfolio.html">portfolio</a>
+            <a id="index" href="index.html">what</a>
+            <a id="portfolio" href="portfolio.html">portfolio</a>
         </div>
     </header>`;
     document.getElementById("app-header").innerHTML = appHeader;
