5a9f30a260c10510ed9f044bfd6e0d083f17a1a7
izzymandias1 <hazardfirefly@gmail.com>
2025-07-24 18:16:15 +0100

    moved header & footer to .js files

---
+++ footer.js
@@ -0,0 +1,13 @@
+let appFooter = `
+<footer class="footertext">
+        <div>
+            <a href="https://bsky.app/profile/izzythewikiwitch.bsky.social">bsky</a>
+            <div class="tooltip"><a href="https://discordapp.com/users/384753654613278722"><i
+                        class="fa-brands fa-discord"></i>discord</a>
+                <span class="tooltiptext">@izzymandias</span>
+            </div>
+        </div>
+        <p class="tiny">i'm gay and you're also gay if you're reading this. sucks to suck bozo.<br />
+            izzythewikiwitch © 2025</p>
+    </footer>`;
+document.getElementById("app-footer").innerHTML = appFooter;
---
+++ header.js
@@ -0,0 +1,13 @@
+    let appHeader = `
+    <header>
+        <img class="logo" src="images/2024_PFP.png"
+            alt="izzy's logo for 2024 to 2025. a picture of a woman with deer antlers in front of the lesbian flag."
+            width="200" />
+        <h1 class="title">izzythewikiwitch</h1>
+        <sub>"what if i made my own wiki. with yuri. and love."</sub>
+        <div class="topnav">
+            <a href="index.html">home</a>
+            <a href="portfolio.html">portfolio</a>
+        </div>
+    </header>`;
+    document.getElementById("app-header").innerHTML = appHeader;
