9ca9d1274c31fb4fc0709172d710d635e518559a
izzymandias1 <hazardfirefly@gmail.com>
2025-07-25 15:00:35 +0100

    added the yuri page. it's so fucking stupid (i love it)

--- footer.js
+++ footer.js
@@ -12,7 +12,17 @@ let appFooter = `
             <div class="buttoncolumn"><img src="images/lesbian.png" alt="Femme (Lesbian flag)"></div>
             <div class="buttoncolumn"><img src="images/sapphic.png" alt="Sapphic (Sapphic flag)"></div>
         </div>
-        <code class="horus">i'm gay and you're also gay if you're reading this. sucks to suck bozo.</code><br />
+        <div class="buttonrow">
+            <div class="buttoncolumn"><img loading="lazy" src="images/buttons/adollzlife_brittany_surprise" alt="adollzlife_brittany_surprise" title="adollzlife_brittany_surprise" height="31" width="88"></div>
+            <div class="buttoncolumn"><img loading="lazy" src="images/buttons/angelkwong_femalesiteonly.gif" alt="angelkwong_femalesiteonly" title="angelkwong_femalesiteonly" height="31" width="88"></div>
+            <div class="buttoncolumn"><img loading="lazy" src="images/buttons/applelicious17_anime2.gif" alt="applelicious17_anime2" title="applelicious17_anime2" height="31" width="88"></div>
+        </div>
+        <div class="buttonrow">
+            <div class="buttoncolumn"><img loading="lazy" src="images/buttons/Area51_Dimension_2375_logos_logo_freespeech_animated.GIF" alt="Area51_Dimension_2375_logos_logo_freespeech_animated" title="Area51_Dimension_2375_logos_logo_freespeech_animated" height="31" width="88"></div>
+            <div class="buttoncolumn"><img loading="lazy" src="images/buttons/Area51_Cavern_8782_800x600.gif" alt="Area51_Cavern_8782_800x600" title="Area51_Cavern_8782_800x600" height="31" width="88"></div>
+            <div class="buttoncolumn"><img loading="lazy" src="images/buttons/Area51_Dimension_2375_logos_logo_eclipse.gif" alt="Area51_Dimension_2375_logos_logo_eclipse" title="Area51_Dimension_2375_logos_logo_eclipse" height="31" width="88"></div>
+        </div>
+        <a href="passtheyuri.html" style="text-decoration: none;"><code class="horus">i'm gay and you're also gay if you're reading this. sucks to suck bozo.</code></a><br />
             <p>izzythewikiwitch © 2025  <a href="smugjug.html"><img src="images/smugjugs/SmugJug.gif" alt="smug jug. he's a jug who is smug." style="width:0.5em;vertical-align: middle;display: inline;"></a></p>
     </footer>`;
 document.getElementById("app-footer").innerHTML = appFooter;
